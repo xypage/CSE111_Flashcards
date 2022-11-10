41afec10a464ac9db3149fa31a7321ba3533ff90
O-Ayala <olmospsnemail1@gmail.com>
2022-11-10 11:41:21 -0800

    v2 schema update

--- fl_schema.sql
+++ fl_schema.sql
@@ -1,44 +1,45 @@
 CREATE TABLE IF NOT EXISTS flashcard (
-    front_id            DECIMAL(4,0) NOT NULL,
-    back_id             DECIMAL(4,0) NOT NULL,
-    correct_count       DECIMAL(4,0) NOT NULL,
-    incorrect_count     DECIMAL(4,0) NOT NULL
+    flashcard_id        INTEGER NOT NULL PRIMARY KEY,
+    front_id            INTEGER NOT NULL,
+    back_id             INTEGER NOT NULL,
+    correct_count       INTEGER NOT NULL,
+    incorrect_count     INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS deck (
-    deck_id             DECIMAL(4,0) NOT NULL,
+    deck_id             INTEGER NOT NULL PRIMARY KEY,
     d_name              VARCHAR(55) NOT NULL,
     d_description       VARCHAR(75) NOT NULL,
     icon_path           VARCHAR(75)
 );
 
 CREATE TABLE IF NOT EXISTS user (
-    user_id             DECIMAL(4,0) NOT NULL,
+    user_id             INTEGER NOT NULL PRIMARY KEY,
     username            VARCHAR(25) NOT NULL,
     u_password          VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS u_session (
-    session_id          DECIMAL(4,0) NOT NULL,
+    session_id          INTEGER NOT NULL PRIMARY KEY,
     cur_date            datetime DEFAULT(getdate()),
-    correct_ratio       DECIMAL(4,0) NOT NULL,
-    sess_userid         DECIMAL(3,0) NOT NULL
+    correct_ratio       INTEGER NOT NULL,
+    sess_userid         INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS spacing (
-    spacing_id          DECIMAL(3,0) NOT NULL,
-    card_id             DECIMAL(3,0) NOT NULL,
-    s_interval          datetime(10) NOT NULL,
-    ef                  DECIMAL(6,3) NOT NULL
+    spacing_id          INTEGER NOT NULL PRIMARY KEY,
+    card_id             INTEGER NOT NULL,
+    s_interval          datetime DEFAULT(getdate()),
+    ef                  INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS categories (
-    category_id         DECIMAL(3,0) NOT NULL,
+    category_id         INTEGER NOT NULL PRIMARY KEY,
     c_name              VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS side (
-    side_id             DECIMAL(3,0) NOT NULL,
+    side_id             INTEGER NOT NULL PRIMARY KEY,
     s_header            VARCHAR(30) NOT NULL,
     s_body              VARCHAR(100) NOT NULL,
     img_path            VARCHAR(30)
